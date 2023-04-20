22bd6db22b54ec5618ecc5ea2950c0d4f2e38da8
Shinryuzz <mshin1127@gmail.com>
2023-04-20 12:54:11 +0900

    add frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI,WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
-from ConnectionManager import *
+# from ConnectionManager import *
 from datetime import datetime
 import json
+from typing import List
+
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, 
@@ -12,7 +15,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app = FastAPI()
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 manager = ConnectionManager()
 
